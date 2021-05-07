f3b1c7f5391dacb78b5d7c6942c920c184b0f8e4
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-05-07 14:17:35 +0200

    initial

---
+++ src/main/java/htwberlin/guenstigertanken/GuenstigertankenApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.guenstigertanken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuenstigertankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuenstigertankenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/guenstigertanken/GuenstigertankenApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.guenstigertanken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuenstigertankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
