8881b0c0254cebffececbc5cd7dac13ae59c4711
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-05-07 15:15:52 +0200

    Initialize to Java16

--- src/main/java/htwberlin/guenstigertanken/GuenstigertankenApplication.java
+++ src/main/java/htwberlin/guenstigertanken/GuenstigertankenApplication.java
@@ -13,6 +13,7 @@ public class GuenstigertankenApplication {
 
 	public static void main(String[] args) {
 		GuenstigertankenApplication app = new GuenstigertankenApplication();
+		System.out.println(app);
 	}
 }
 
