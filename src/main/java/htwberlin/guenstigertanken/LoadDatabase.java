2d71470bd2d580be4c6feaaf88167172b6bcbf5e
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-05-08 13:14:14 +0200

    add tanken

--- src/main/java/htwberlin/guenstigertanken/LoadDatabase.java
+++ src/main/java/htwberlin/guenstigertanken/LoadDatabase.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
