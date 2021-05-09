7471c0dfd4bc1b42f3ae4d09b2994be55dfae566
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-05-09 14:09:15 +0200

    hard coded aral changed to Aral

--- src/main/java/htwberlin/guenstigertanken/GuenstigertankenController.java
+++ src/main/java/htwberlin/guenstigertanken/GuenstigertankenController.java
@@ -14,7 +14,7 @@ public class GuenstigertankenController {
 	}
 	public GuenstigertankenController(){
 		this.tanken = new ArrayList<Tanken>();
-		this.tanken.add(new Tanken(1,"2021-05-17 15:07:00","aral", "Heilbronn",10.5,1.7));
+		this.tanken.add(new Tanken(1,"2021-05-17 15:07:00","Aral", "Heilbronn",10.5,1.7));
 	}
 	@GetMapping("/")
 	public String hello() {
