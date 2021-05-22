2937a5797b28a21b806f69657f648f426e5938a4
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-05-22 21:02:12 +0200

    changed GenerationType to Identify

--- src/main/java/htwberlin/guenstigertanken/Tanken.java
+++ src/main/java/htwberlin/guenstigertanken/Tanken.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Table(name="Tanken")
 public class Tanken {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     @Column(name="date")
     private Timestamp date;
