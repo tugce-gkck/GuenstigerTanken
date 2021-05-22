d989bd5e019a92216f7ef6e3d83167873e1868c2
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-05-22 15:53:20 +0200

    Added h2database to dep.

--- src/main/java/htwberlin/guenstigertanken/Application.java
+++ src/main/java/htwberlin/guenstigertanken/Application.java
@@ -3,6 +3,10 @@ package htwberlin.guenstigertanken;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 
 @SpringBootApplication
