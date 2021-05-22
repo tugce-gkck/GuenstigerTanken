4102e3d2029d1038e94170295e207f61b3d6142e
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-05-22 19:53:05 +0200

    Changed Dependencies

--- src/main/java/htwberlin/guenstigertanken/TankenRepository.java
+++ src/main/java/htwberlin/guenstigertanken/TankenRepository.java
@@ -1,11 +1,13 @@
 package htwberlin.guenstigertanken;
 
+import java.sql.Timestamp;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TankenRepository extends CrudRepository<Tanken, Long> {
 
-    List<Tanken> findByLastName(String lastName);
+    List<Tanken> findByDate(Timestamp ts);
+    List<Tanken> findByName(String name);
 
     Tanken findById(long id);
 }
