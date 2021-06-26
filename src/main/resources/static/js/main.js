c2be8c8c08159a10bb085565c61680fbebf8a468
Tugce Gökcek <Tugce.Goekcek@Student.HTW-Berlin.de>
2021-06-26 23:22:24 +0200

    replace points with comma in distance and price

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -51,9 +51,9 @@ var tanken = new Vue({
                                 comp.TankenArray[i].date = date.toLocaleDateString('de-DE', options);
 
                                 var price = comp.TankenArray[i].price.toString();
-                                comp.TankenArray[i].price = price.replace(',','.').replace(' ','')
+                                comp.TankenArray[i].price = price.replace('.',',').replace(' ','')
                                 var distance = comp.TankenArray[i].distance.toString();
-                                comp.TankenArray[i].distance = distance.replace(',','.').replace(' ','')
+                                comp.TankenArray[i].distance = distance.replace('.',',').replace(' ','')
                             }
                         }
                     } else {
